3f82aee34bf6734707b1073f8896d03589fb217a
Thomas Cottis <tcottis@domain.dme>
2024-12-23 22:23:43 -0700

    Created test to connect ensure database connection
    Next is to build out support for password handling
    
    Password handling includes CRUD + hashing/validating
    
    This will lead into the next section to build out; AUTH

--- src/test/java/com/atletico/atletico_revamp/AtleticoRevampApplicationTests.java
+++ src/test/java/com/atletico/atletico_revamp/AtleticoRevampApplicationTests.java
@@ -1,13 +1,31 @@
 package com.atletico.atletico_revamp;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AtleticoRevampApplicationTests {
 
+	@Autowired
+	private DataSource dataSource;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testDatabaseConnection() throws Exception {
+		try (Connection connection = dataSource.getConnection()) {
+			assertNotNull(connection, "Database connection should not be null");
+			System.out.println("Database connected successfully: " + connection.getMetaData().getURL());
+		}
 	}
 
 }
